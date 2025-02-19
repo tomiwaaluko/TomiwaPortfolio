cb7fa43c3fb1c46d16f862448aef9ddd463e871d
Tomiwa Aluko <tomiwaaluko02@gmail.com>
2025-02-18 21:21:09 -0500

    Added Achievements Section & Footer. Edited EmailSection & route.js

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,27 +1,30 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
+import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.re_W9C3LepG_zwcvSksv8m5pg2jsrdJU3K8E);
+// const resend = new Resend(process.env.re_W9C3LepG_zwcvSksv8m5pg2jsrdJU3K8E);
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST() {
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
   try {
-    const { data, error } = await resend.emails.send({
-      from: "Tomiwa <tomiwaaluko02@gmail.com",
-      to: ["tomiwaaluko02@gmail.com"],
-      subject: "Hello world",
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
       react: (
         <>
-          <p>Email Body</p>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
         </>
       ),
     });
-
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
-
-    return Response.json(data);
+    return NextResponse.json(data);
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return NextResponse.json({ error });
   }
 }
