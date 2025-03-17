879ea8e0c8e44bd01d40abff84905a699058876a
Tomiwa Aluko <tomiwaaluko02@gmail.com>
2025-03-17 16:52:38 -0400

    Working on ScrollLinked section of the website.

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
-import ProjectSection from "./components/ProjectSection"; // Add this import
+import ProjectSection from "./components/ProjectSection";
+import ScrollLinked from "./components/ScrollLinked"; // Import ScrollLinked component
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 import AchievementsSection from "./components/AchievementsSection";
@@ -16,6 +17,10 @@ export default function Home() {
         <AchievementsSection />
         <AboutSection />
         <ProjectSection />
+
+        {/* Add ScrollLinked Component Below Projects Section */}
+        <ScrollLinked />
+
         <EmailSection />
       </div>
       <Footer />
